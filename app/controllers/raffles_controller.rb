3e39578d9928da4cfa5f3f4c1c5057c985e5aa41
Marisol Smith <marisolsmith@gmail.com>
2018-09-12 20:44:02 -0300

    contador navbar y boton eliminar rifas

--- app/controllers/raffles_controller.rb
+++ app/controllers/raffles_controller.rb
@@ -3,13 +3,21 @@ class RafflesController < ApplicationController
   def create
     @product = Product.find(params[:product_id])
     @raffle = Raffle.new(product: @product, user: current_user)
-    if @raffle.save
-      redirect_to products_path, notice: 'Rifa añadida al carro de compras'
-    else
-      redirect_to products_path, alert: 'Tu rifa no ha sido añadida al carro de compras'
-    end
+      if @raffle.save
+        redirect_to products_path, notice: 'Rifa añadida al carro de compras'
+      else
+        redirect_to products_path, alert: 'Tu rifa no ha sido añadida al carro de compras'
+      end
   end
+
   def index
     @raffles = current_user.raffles
   end
+
+  def destroy
+    @raffle = Raffle.find(params[:id])
+    @raffle.destroy
+    redirect_to raffles_path
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
     resources :raffles, only: :create
   end
   resources :raffles, only: :index
+  delete 'raffles/:id', to: 'raffles#destroy', as: 'raffles/delete'
   root to: 'products#index'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
